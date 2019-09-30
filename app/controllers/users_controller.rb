2e73c65ace2c457da49f08a4d9eb148b6bcb385d
Yuk1M1yag1 <tengiya228mi.joh@docomo.ne.jp>
2019-09-30 14:51:11 +0000

    fix users_controller.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,5 @@
 
 class UsersController < ApplicationController
   @user = User.new(params[:user].permit(:name))
-  if verify_recaptcha(model: @user) && @user.save
-    redirect_to @user
-  else
-    render 'new'
-  end
+  
 end
